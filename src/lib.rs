3bd467b382c8c2a655b03d9a1e80911bce42e513
Cole Kauder-McMurrich <gamingwithcole7@gmail.com>
2023-11-19 14:07:07 -0500

    Initial paging implementation

--- src/lib.rs
+++ src/lib.rs
@@ -2,13 +2,17 @@
 #![cfg_attr(test, no_main)]
 #![feature(custom_test_frameworks)]
 #![test_runner(crate::test_runner)]
-#![reexport_test_harness_main = "test_main"]
+#![reexport_test_harness_main = "test_harness_main"]
 #![feature(abi_x86_interrupt)]
 
 use core::panic::PanicInfo;
 
+#[cfg(test)]
+use bootloader::{entry_point, BootInfo};
+
 pub mod gdt;
 pub mod interrupts;
+pub mod memory;
 pub mod serial;
 pub mod vga_driver;
 
@@ -42,12 +46,15 @@ pub fn test_panic_handler(info: &PanicInfo) -> ! {
     hlt_loop();
 }
 
+#[cfg(test)]
+entry_point!(test_main);
+
 /// Entry point for `cargo test`
 #[cfg(test)]
 #[no_mangle]
-pub extern "C" fn _start() -> ! {
+fn test_main(boot_info: &'static BootInfo) -> ! {
     init();
-    test_main();
+    test_harness_main();
     hlt_loop();
 }
 
--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,34 @@
 #![test_runner(diy_os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
+use bootloader::{entry_point, BootInfo};
 use core::panic::PanicInfo;
-use diy_os::{init, println, hlt_loop};
+use diy_os::{
+    hlt_loop, init,
+    memory::{self, BootInfoFrameAllocator},
+    println,
+};
+use x86_64::structures::paging::{Page, PageTable, Translate};
+
+entry_point!(main);
 
 #[no_mangle]
-pub extern "C" fn _start() -> ! {
+fn main(boot_info: &'static BootInfo) -> ! {
+    use x86_64::VirtAddr;
+
     println!("hello world");
     init();
+
+    let physicals_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+
+    let mut mapper = unsafe { diy_os::memory::init(physicals_mem_offset) };
+    let mut allocator = unsafe { BootInfoFrameAllocator::init(&boot_info.memory_map) };
+
+    let page = Page::containing_address(VirtAddr::new(0));
+    memory::create_example_mapping(page, &mut mapper, &mut allocator);
+
+    let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
+    unsafe { page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e) };
 
     #[cfg(test)]
     test_main();
