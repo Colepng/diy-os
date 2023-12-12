26cd2b942b3e21d7b162eec4f47d848c0b31e1ff
Cole Kauder-McMurrich <gamingwithcole7@gmail.com>
2023-12-12 09:59:00 -0500

    feat: Allow ?Sized types in spinlocks

--- src/spinlock.rs
+++ src/spinlock.rs
@@ -1,28 +1,37 @@
-use core::{sync::atomic::{AtomicBool, Ordering}, cell::UnsafeCell, ops::{Deref, DerefMut}, fmt};
+use core::{
+    cell::UnsafeCell,
+    fmt,
+    ops::{Deref, DerefMut},
+    sync::atomic::{AtomicBool, Ordering},
+};
 
-pub struct Spinlock<T> {
-    data: UnsafeCell<T>,
+pub struct Spinlock<T: ?Sized> {
     locked: AtomicBool,
     interrupts_enabled: UnsafeCell<Option<bool>>,
+    data: UnsafeCell<T>,
 }
 
 impl<T> Spinlock<T> {
     pub const fn new(data: T) -> Self {
         Self {
-            data: UnsafeCell::new(data), 
             locked: AtomicBool::new(false),
             interrupts_enabled: UnsafeCell::new(None),
+            data: UnsafeCell::new(data),
         }
     }
+}
 
+impl<T: ?Sized> Spinlock<T> {
     pub fn acquire(&self) -> SpinlockGuard<'_, T> {
         self.disable_interrupts();
-            // loops until not locked
-            while let Err(_) = self.locked.compare_exchange(false, true, Ordering::SeqCst, Ordering::Acquire) {}
+        // loops until not locked
+        while self
+            .locked
+            .compare_exchange(false, true, Ordering::SeqCst, Ordering::Acquire)
+            .is_err()
+        {}
 
-        SpinlockGuard {
-            spinlock: self
-        }
+        SpinlockGuard { spinlock: self }
     }
 
     fn enable_interrupts(&self) {
@@ -51,47 +60,42 @@ impl<T> Spinlock<T> {
 unsafe impl<T: ?Sized + Send> Send for Spinlock<T> {}
 unsafe impl<T: ?Sized + Send> Sync for Spinlock<T> {}
 
-pub struct SpinlockGuard<'a, T> {
-    spinlock: &'a Spinlock<T>
+pub struct SpinlockGuard<'a, T: ?Sized> {
+    spinlock: &'a Spinlock<T>,
 }
 
 unsafe impl<T: ?Sized + Sync> Sync for SpinlockGuard<'_, T> {}
 impl<T: ?Sized> !Send for SpinlockGuard<'_, T> {}
 
-impl<T> Drop for SpinlockGuard<'_, T> {
+impl<T: ?Sized> Drop for SpinlockGuard<'_, T> {
     #[inline]
     fn drop(&mut self) {
         self.spinlock.release();
     }
 }
 
-impl<T> Deref for SpinlockGuard<'_, T> {
+impl<T: ?Sized> Deref for SpinlockGuard<'_, T> {
     type Target = T;
 
     fn deref(&self) -> &Self::Target {
-        unsafe {
-            &*self.spinlock.data.get()
-        }
+        unsafe { &*self.spinlock.data.get() }
     }
 }
 
-impl<T> DerefMut for SpinlockGuard<'_, T> {
+impl<T: ?Sized> DerefMut for SpinlockGuard<'_, T> {
     fn deref_mut(&mut self) -> &mut Self::Target {
-        unsafe {
-            &mut *self.spinlock.data.get()
-        }
+        unsafe { &mut *self.spinlock.data.get() }
     }
 }
 
-impl<T: fmt::Debug> fmt::Debug for SpinlockGuard<'_, T> {
+impl<T: fmt::Debug + ?Sized> fmt::Debug for SpinlockGuard<'_, T> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         fmt::Debug::fmt(&**self, f)
     }
 }
 
-impl<T: fmt::Display> fmt::Display for SpinlockGuard<'_, T> {
+impl<T: fmt::Display + ?Sized> fmt::Display for SpinlockGuard<'_, T> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         (**self).fmt(f)
     }
 }
-
