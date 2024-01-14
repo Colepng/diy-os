bef256ec9ef0055b48780eac34b73c0396d2a44c
Cole Kauder-McMurrich <gamingwithcole7@gmail.com>
2024-01-13 19:57:15 -0500

    Pass args to qemu

--- src/qemu-bios.rs
+++ src/qemu-bios.rs
@@ -7,6 +7,7 @@ fn main() {
     let mut qemu = Command::new("qemu-system-x86_64");
     qemu.arg("-drive");
     qemu.arg(format!("format=raw,file={}", env!("BIOS_IMAGE")));
+    qemu.args(env::args().skip(1));
     let exit_status = qemu.status().unwrap();
     process::exit(exit_status.code().unwrap_or(-1));
 }
--- src/qemu-uefi.rs
+++ src/qemu-uefi.rs
@@ -8,6 +8,8 @@ fn main() {
     qemu.arg("-drive");
     qemu.arg(format!("format=raw,file={}", env!("UEFI_IMAGE")));
     qemu.arg("-bios").arg(ovmf_prebuilt::ovmf_pure_efi());
+    qemu.args(env::args().skip(1));
+    println!("{:?}", env::args().skip(1).collect::<Vec<String>>());
     let exit_status = qemu.status().unwrap();
     process::exit(exit_status.code().unwrap_or(-1));
 }
