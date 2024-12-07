e158000f8dd990fedcf80abaacb76db467a3013c
Cole Kauder-McMurrich <gamingwithcole7@gmail.com>
2024-12-07 01:17:58 -0500

    Got a cyclo going

--- diy-os/src/multitasking.rs
+++ diy-os/src/multitasking.rs
@@ -33,3 +33,42 @@ impl TaskRunner {
 pub trait Task {
     fn run(&mut self);
 }
+
+pub mod rewrite {
+    use core::{arch::asm, mem::MaybeUninit};
+
+    use alloc::boxed::Box;
+
+    #[derive(Debug)]
+    #[repr(C)]
+    pub struct Task {
+        rax: u64,
+        pub stack: u64,
+    }
+
+    impl Task {
+        pub fn new(rax: u64, stack: u64) -> Task {
+            Self {
+                rax,
+                stack,
+            }
+        }
+
+        pub fn allocate_task(rax: u64, stack: u64) -> Box<Task> {
+            let task: Box<MaybeUninit<Task>> = Box::new_uninit();
+
+            unsafe {
+                asm!(
+                    "mov [{task_ptr}], rax",
+                    "mov [{task_ptr}+8], {stack}",
+                    task_ptr = in(reg) task.as_ptr(),
+                    stack = in(reg) stack,
+                    in("rax") rax,
+                    )
+            }
+
+            return unsafe { task.assume_init() }
+        }
+    }
+
+}
