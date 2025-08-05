a38e63c15ecc271426d132afb745eb79472ea46e
Cole Kauder-McMurrich <gamingwithcole7@gmail.com>
2025-08-05 03:21:30 -0400

    Mount virtual disk as a ramdisk

--- runners/build.rs
+++ runners/build.rs
@@ -6,13 +6,13 @@ fn main() {
 
     // set by cargo for the kernel artifact dependency
     let kernel_path = env::var("CARGO_BIN_FILE_KERNEL").unwrap();
-    let disk_builder = DiskImageBuilder::new(PathBuf::from(kernel_path));
+    let mut disk_builder = DiskImageBuilder::new(PathBuf::from(kernel_path));
 
     // specify output paths
     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
     let uefi_path = out_dir.join("os-uefi.img");
 
-    // disk_builder.set_ramdisk("bin/hello_world.tar".into());
+    disk_builder.set_ramdisk("sfs.img".into());
 
     // create the disk images
     disk_builder.create_uefi_image(&uefi_path).unwrap();
