11e39ea7061a8342bc20b8ce93f6c6169097ba52
Cole Kauder-McMurrich <gamingwithcole7@gmail.com>
2025-08-08 00:07:56 -0400

    Stop building old bios img

--- runners/build.rs
+++ runners/build.rs
@@ -11,15 +11,12 @@ fn main() {
     // specify output paths
     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
     let uefi_path = out_dir.join("os-uefi.img");
-    let bios_path = out_dir.join("os-bios.img");
 
     // disk_builder.set_ramdisk("bin/hello_world.tar".into());
 
     // create the disk images
     disk_builder.create_uefi_image(&uefi_path).unwrap();
-    disk_builder.create_bios_image(&bios_path).unwrap();
 
     // pass the disk image paths via environment variables
     println!("cargo:rustc-env=UEFI_IMAGE={}", uefi_path.display());
-    println!("cargo:rustc-env=BIOS_IMAGE={}", bios_path.display());
 }
