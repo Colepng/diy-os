16e73dc5e37dc1c873c8c45719202f6ee26743c1
Cole Kauder-McMurrich <gamingwithcole7@gmail.com>
2026-06-11 19:52:54 -0400

    Fix asm to get into usermode and GDT

--- diy-os-lib/src/gdt.rs
+++ diy-os-lib/src/gdt.rs
@@ -8,18 +8,21 @@ pub const DOUBLE_FAULT_IST_INDEX: u16 = 0;
 use x86_64::structures::gdt::{Descriptor, GlobalDescriptorTable};
 
 lazy_static! {
-    static ref GDT: (GlobalDescriptorTable, Selectors) = {
+    pub static ref GDT: (GlobalDescriptorTable, Selectors) = {
         let mut gdt = GlobalDescriptorTable::new();
-        let code_selector = gdt.append(Descriptor::kernel_code_segment());
-        let _kernal_data = gdt.append(Descriptor::kernel_data_segment());
-        let _user_code = gdt.append(Descriptor::user_code_segment());
-        let _user_data = gdt.append(Descriptor::user_data_segment());
+        let kernel_code_selector = gdt.append(Descriptor::kernel_code_segment());
+        let kernel_data_selector = gdt.append(Descriptor::kernel_data_segment());
+        let user_data_selector = gdt.append(Descriptor::user_data_segment());
+        let user_code_selector = gdt.append(Descriptor::user_code_segment());
         #[allow(static_mut_refs)]
         let tss_selector = gdt.append(Descriptor::tss_segment(unsafe { &TSS }));
         (
             gdt,
             Selectors {
-                code_selector,
+                kernel_code_selector,
+                kernel_data_selector,
+                user_code_selector,
+                user_data_selector,
                 tss_selector,
             },
         )
@@ -31,24 +34,37 @@ const STACK_SIZE: usize = 4096 * 5;
 // TODO: fucking fix this dumbass
 // just allocate some pages
 static mut STACK: [u8; STACK_SIZE] = [0; STACK_SIZE];
+static mut PRIVILEGE_STACK: [u8; STACK_SIZE] = [0; STACK_SIZE];
 
 pub static mut TSS: TaskStateSegment = TaskStateSegment::new();
 
 pub fn init() {
-    (unsafe { TSS.interrupt_stack_table })[DOUBLE_FAULT_IST_INDEX as usize] = {
-        let stack_start = VirtAddr::from_ptr(&raw mut STACK);
-        stack_start + STACK_SIZE as u64
-    };
+    unsafe {
+        TSS.interrupt_stack_table[DOUBLE_FAULT_IST_INDEX as usize] = {
+            let stack_start = VirtAddr::from_ptr(&raw mut STACK);
+            stack_start + STACK_SIZE as u64
+        };
+    }
+
+    unsafe {
+        TSS.privilege_stack_table[0] = {
+            let stack_start = VirtAddr::from_ptr(&raw mut PRIVILEGE_STACK);
+            stack_start + STACK_SIZE as u64
+        };
+    }
 
     GDT.0.load();
 
     unsafe {
-        CS::set_reg(GDT.1.code_selector);
+        CS::set_reg(GDT.1.kernel_code_selector);
         load_tss(GDT.1.tss_selector);
     }
 }
 
-struct Selectors {
-    code_selector: SegmentSelector,
-    tss_selector: SegmentSelector,
+pub struct Selectors {
+    pub kernel_code_selector: SegmentSelector,
+    pub kernel_data_selector: SegmentSelector,
+    pub user_code_selector: SegmentSelector,
+    pub user_data_selector: SegmentSelector,
+    pub tss_selector: SegmentSelector,
 }
--- diy-os-lib/src/usermode.rs
+++ diy-os-lib/src/usermode.rs
@@ -1,58 +1,27 @@
-use core::arch::asm;
+use core::arch::naked_asm;
 
-pub extern "sysv64" fn into_usermode(entry: u64, stack_addr: u64) {
-    unsafe {
-        asm!(
+/// Jumps to `entry` in ring 3, setting the stack, with interrupts enabled
+///
+/// # Safety
+/// The page where `entry` is mapped readable, user accessible and present.
+/// The data on the page must also be valid code.
+///
+/// `stack_addr` is mapped, read/writeable, user accessible and present.
+///
+/// Both addressing must be in the lower half.
+// Entry is passed in rd
+// stack rsi
+#[unsafe(naked)]
+pub unsafe extern "sysv64" fn into_usermode(entry: u64, stack_addr: u64) {
+    naked_asm!(
         "cli",
-        // rdi = user args
-        // rsi = entry point for userspace
-        // rdx = user space stack
-        // "mov rax, 0x18 | 3",
-        // "mov ax, ( 4 * 8 ) | 3",
-        // "mov ds, ax",
-        // "mov es, ax",
-        // "mov fs, ax",
-        // "mov gs, ax",
-
-        // "push rax", // user data
-        // "push rsp", // user stack
-        // "pushf", // rflags = inerrupts + reservied bit
-        // "push 0x23", // selctor 0x20 + rpl 3
-        // "push {}", // entry point
-        // fake iret frame
-         "mov ax, (4 * 8) | 3",
-         "mov ds, ax",
-         "mov es, ax",
-         "mov fs, ax",
-         "mov gs, ax",
-         // //stackfame
-         "mov rax, {1}",
-         "push (4 * 8) | 3 ",
-         "push rax",
-         "push 0x202",
-         "push ( 3 * 8 ) | 3",
-         "push {0}",
-         "iretq",
-         in(reg) entry,
-         in(reg) stack_addr,
-         options(noreturn),
-        )
-    }
+        "push {user_ss}",
+        "push rsi",
+        "push 0x202",
+        "push {user_cs}",
+        "push rdi",
+        "iretq",
+        user_ss = const 0x1B,
+        user_cs = const 0x23,
+    );
 }
-
-// #[no_mangle]
-// pub extern "C" fn usermode() {
-//     unsafe {
-//         asm!(
-//             "mov rax, 1",
-//             "mov rdx, 35",
-//             "mov rsi, 30",
-//             "int 0x80",
-//             "push rax",
-//             "mov rax, 0",
-//             "mov rsi, rsp",
-//             "mov rdx, 1",
-//             "int 0x80",
-//         );
-//     }
-// }
